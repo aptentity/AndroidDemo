94bc975f2e5a80b1eb10c1c6b41619e210145dee
feilong.zfl <feilong.zfl@alibaba-inc.com>
2015-11-17 20:28:11 +0800

    add intent service demo

---
+++ app/src/main/java/com/borg/androidemo/activity/IntentServiceTestActivity.java
@@ -0,0 +1,30 @@
+package com.borg.androidemo.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.borg.androidemo.R;
+import com.borg.androidemo.common.log.LogHelper;
+import com.borg.androidemo.service.TestIntentService;
+
+public class IntentServiceTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_service_test);
+
+        LogHelper.d("IntentServiceTestActivity start");
+        Intent i = new Intent(this, TestIntentService.class);
+        i.putExtra("name", "task1");
+        startService(i);
+        i.putExtra("name", "task2");
+        startService(i);
+        i.putExtra("name", "task3");
+        startService(i);
+        i.putExtra("name", "task4");
+        startService(i);
+        LogHelper.d("IntentServiceTestActivity end");
+    }
+}
---
+++ app/src/main/java/com/borg/androidemo/service/TestIntentService.java
@@ -0,0 +1,50 @@
+package com.borg.androidemo.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import com.borg.androidemo.common.log.LogHelper;
+
+/**
+ * Created by Gulliver(feilong) on 15/11/17.
+ */
+public class TestIntentService extends IntentService{
+    public TestIntentService(){
+        super("TestIntentService");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LogHelper.d("TestIntentService onCreate"+ ";threadid=" + Thread.currentThread().getId());
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        LogHelper.d("TestIntentService onStart startId=" + startId + ";threadid=" + Thread.currentThread().getId());
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        LogHelper.d("TestIntentService onStartCommand startId="+startId+ ";threadid=" + Thread.currentThread().getId());
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String action = intent.getStringExtra("name");
+        try {
+            Thread.sleep(3000);
+        }catch (Exception e){}
+
+        LogHelper.d("TestIntentService onHandleIntent action="+action+ ";threadid=" + Thread.currentThread().getId());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        LogHelper.d("TestIntentService onDestroy"+ ";threadid=" + Thread.currentThread().getId());
+    }
+}
