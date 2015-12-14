2d35f57206c69dfe5e8ab095b4427718ba9aea44
feilong.zfl <feilong.zfl@alibaba-inc.com>
2015-12-14 17:17:01 +0800

    add waitdlg

---
+++ app/src/main/java/com/borg/mvp/view/widget/WaitDlg.java
@@ -0,0 +1,27 @@
+package com.borg.mvp.view.widget;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+
+/**
+ * Created by Gulliver(feilong) on 15/12/14.
+ */
+public class WaitDlg {
+    private ProgressDialog m_pDialog;
+    public WaitDlg(Activity activity){
+        m_pDialog = new ProgressDialog(activity);
+        m_pDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+
+        m_pDialog.setIndeterminate(false);
+        m_pDialog.setCancelable(false);
+    }
+
+    public void show(String msg){
+        m_pDialog.setMessage(msg);
+        m_pDialog.show();
+    }
+
+    public void hide(){
+        m_pDialog.hide();
+    }
+}
