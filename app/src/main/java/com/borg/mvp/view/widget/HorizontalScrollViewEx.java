ad688d9b812f5ceea4907609029ac3c9f908f597
feilong.zfl <feilong.zfl@alibaba-inc.com>
2016-01-20 10:19:35 +0800

    滑动

--- app/src/main/java/com/borg/mvp/view/widget/HorizontalScrollViewEx.java
+++ app/src/main/java/com/borg/mvp/view/widget/HorizontalScrollViewEx.java
@@ -2,8 +2,11 @@ package com.borg.mvp.view.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Scroller;
 
 /**
  * Created by Gulliver(feilong) on 16/1/19.
@@ -13,13 +16,128 @@ public class HorizontalScrollViewEx extends ViewGroup{
     private int mChildrenSize;
     private int mChildWidth;
     private int mChildIndex;
+    private Scroller mScroller;
+    private VelocityTracker mVelocityTracker;
+
+    // 记录上次滑动的坐标
+    private int mLastX = 0;
+    private int mLastY = 0;
+    // 记录上次滑动的坐标
+    private int mLastXIntercept = 0;
+    private int mLastYIntercept = 0;
 
     public HorizontalScrollViewEx(Context context){
         super(context);
+        init();
     }
 
     public HorizontalScrollViewEx(Context context,AttributeSet attrs){
         super(context, attrs);
+        init();
+    }
+
+    public HorizontalScrollViewEx(Context context,AttributeSet attrs,int defStyle){
+        super(context,attrs,defStyle);
+        init();
+    }
+
+    private void init(){
+        if (mScroller==null){
+            mScroller = new Scroller(getContext());
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean intercepted = false;
+        int x = (int)ev.getX();
+        int y = (int)ev.getY();
+        switch (ev.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                intercepted = false;
+                if (!mScroller.isFinished()){
+                    mScroller.abortAnimation();
+                    intercepted = true;
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x-mLastXIntercept;
+                int deltaY = y-mLastYIntercept;
+                if (Math.abs(deltaX)>Math.abs(deltaY)){
+                    intercepted = true;
+                }else {
+                    intercepted = false;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                intercepted = false;
+                break;
+            default:
+                break;
+        }
+        mLastX = x;
+        mLastY = y;
+        mLastXIntercept = x;
+        mLastYIntercept = y;
+        return intercepted;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mVelocityTracker.addMovement(event);
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                if (!mScroller.isFinished()){
+                    mScroller.abortAnimation();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - mLastX;
+                int deltaY = y - mLastY;
+                scrollBy(-deltaX,0);
+                break;
+            case MotionEvent.ACTION_UP:
+                int scrollX = getScrollX();
+                mVelocityTracker.computeCurrentVelocity(1000);
+                float xVelocity = mVelocityTracker.getXVelocity();
+                if (Math.abs(xVelocity)>=50){
+                    mChildIndex = xVelocity>0?mChildIndex-1:mChildIndex+1;
+                }else {
+                    mChildIndex = (scrollX + mChildIndex/2)/mChildWidth;
+                }
+                mChildIndex = Math.max(0,Math.min(mChildIndex,mChildrenSize-1));
+                int dx = mChildIndex*mChildWidth-scrollX;
+                smoothScrollBy(dx,0);
+                break;
+            default:
+                break;
+        }
+        mLastX = x;
+        mLastY = y;
+        return true;
+    }
+
+
+    private void smoothScrollBy(int dx,int dy){
+        mScroller.startScroll(getScrollX(),0,dx,0,500);
+        invalidate();
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()){
+            scrollTo(mScroller.getCurrX(),mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        mVelocityTracker.recycle();
+        super.onDetachedFromWindow();
     }
 
     /**
